2ceecefba887b51f60038b694cb4987c2719f298
brunomioto <brunomioto97@gmail.com>
2021-09-03 01:52:40 -0300

    Add files via upload

--- R/zzz.R
+++ R/zzz.R
@@ -1,19 +1,11 @@
-.onAttach <-function(lib,pkg)
+.onAttach <-function(...)
 {
-  ver <- read.dcf(file.path(lib, pkg, "DESCRIPTION"), "Version")
-  ver <- as.character(ver)	
-  
-  curdate <- read.dcf(file.path(lib, pkg, "DESCRIPTION"), "Date")
-  curdate <- as.character(curdate)
-  
   packageStartupMessage(paste(
     "\n",
-    "*****************************************************************\n",
+    "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n",
     "reservatoriosBR: Um pacote de dados de reservatórios brasileiros\n\n",
     "               --- Criado por Bruno Mioto ---\n",
-    "  Versão atual do pacote: ",ver," (",curdate,") \n\n",
-    "\n",
-    "******************************************************************")
+    "________________________________________________________________")
   )
 
 }
